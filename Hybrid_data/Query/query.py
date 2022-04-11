6c6994f162b217fe39272a68aa91d3d2199841a9
Rohit Gupta <55106484+rohit252@users.noreply.github.com>
2022-04-11 18:09:27 +0200

    Rdf transformation of Hybrid data

---
+++ Hybrid_data/Query/query.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 29 15:34:52 2022
+
+@author: GuptaR
+"""
+
+# -*- coding: utf-8 -*-
+
+# import libraries
+
+from SPARQLWrapper import SPARQLWrapper, XML, JSON, TURTLE, CSV
+import requests
+import time
+import pandas as pd
+
+
+''' query1 '''
+
+start = time.time()
+
+
+sparql = SPARQLWrapper("http://localhost:7200/repositories/Hybrid")
+
+
+# sparql.setMethod(POST)
+
+ 
+sparql.setQuery("""PREFIX hybrid3: <https://materials.hybrid3.duke.edu/materials/> 
+PREFIX matonto: <http://matonto.org/ontology/matonto#> 
+PREFIX owl: <http://www.w3.org/2002/07/owl#> 
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+PREFIX sosa: <http://www.w3.org/ns/sosa/> 
+PREFIX ssn: <http://www.w3.org/ns/ssn/> 
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+PREFIX rml: <http://semweb.mmlab.be/ns/rml#>
+PREFIX ql: <http://semweb.mmlab.be/ns/ql#> 
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+
+SELECT (count(*) as ?triples)
+
+where{
+  ?id a sosa:Sample;
+      ?s1 ?o1.
+    
+}
+
+""")
+
+results = sparql.query()
+# print(results)
+sparql.setReturnFormat(JSON)
+results_query1 = sparql.query().convert()
+
+
+end = time.time()
+
+print('time taken to execute the query',end -   start)
+
+
+
+''' storing results locally '''
+query1=[]
+for result in results_query1["results"]["bindings"]:
+    query1.append((result["triples"]["value"]))
+    with open('query1.txt', 'w') as f:
+        for s in query1:
+            f.write(str(s)+'\n')
+    
+    
+    
+    
+# with open('query1.txt', 'w') as f:
+#     for s in query1:
+#         f.write(str(s)[1:-1]+'\n')   
+
+
+
+    
+        
+
+
---
+++ Hybrid_data/db_to_csv/db_to_csv.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 11 14:54:40 2022
+
+@author: GuptaR
+"""
+
+
+
+
+import os 
+
+import sys
+
+# import pypyodbc as podbc
+
+import pandas as pd 
+
+import mysql.connector
+
+from rdflib import Graph
+
+from pathlib import Path
+
+import pickle
+
+import configparser
+
+
+
+
+""" reading the path from path.properties file,specify the path of path.properties file """
+
+config = configparser.ConfigParser()
+
+''' specify the path of path.properties file here '''
+
+config.readfp(open('./tables.properties'))
+
+
+
+
+
+''' taking tables name '''
+
+table1 = config.get('TABLES','materials_dataset')
+
+
+# ''' taking queries '''
+
+# query1 = config.get('QUERIES','query1')
+
+print(table1)
+
+
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = '8227',
+    database= 'hybrid')
+
+# print(mydb)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("select database();")
+
+database = mycursor.fetchone()
+
+print(database)
+
+
+# i = 0
+    
+for each_section in config.sections(): 
+    for each_key, each_val in config.items(each_section):
+        
+        query = each_val
+    
+       
+        print(query)
+            
+  
+        # query = 'select * from materials_dataset' 
+        
+        results = pd.read_sql_query(query, mydb)
+        
+        if each_key == 'materials_dataset':
+            
+            results['secondary_property_id'] =  results['secondary_property_id'].astype('Int64')
+            results.to_csv(each_key + ".csv", index=False)
+            
+        else:
+            
+            
+            results.to_csv(each_key + ".csv", index=False)
+            
+
+        
+
+
+
